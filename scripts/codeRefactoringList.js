286eb1bb1fdb09e816a466cceefddd547b4f5f5a
sergey <oborok05@bk.ru>
2023-09-29 09:35:51 +0300

    finish v0.1.3

--- scripts/codeRefactoringList.js
+++ scripts/codeRefactoringList.js
@@ -1,4 +1,4 @@
-const reservedWordsPython = ["from", "import", "class", "True", "False", "def"]
+const reservedWordsPython = ["from", "import", "class", "True", "False", "def", "pass"]
 const reservedWordsPyAlice = ["pyAlice", "pyalice", "pyAlice.pyalice",
                             "key_words.key_words", "key_words",
                             "buttons.buttons", "buttons"]
